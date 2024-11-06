24ae24dc85efe35f5da45b96c73d11ad5a669f2f
Abhishek6546 <abhishekk11603@gmail.com>
2024-11-06 15:43:08 +0530

    envchanges

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,6 +2,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url:'postgresql://MockMate_owner:ZDTkd0f9lCaO@ep-falling-band-a15h55ls.ap-southeast-1.aws.neon.tech/MockMate?sslmode=require',
+      url: process.env.DRIZZLE_DB_URL,
     }
   };
